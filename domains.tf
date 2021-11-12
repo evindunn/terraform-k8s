205ba9ca1e1929fa7ff9f1a878e26f64f0b733e9
Evin Dunn <evindunn14@gmail.com>
2021-11-12 10:17:17 -0700

    Adds more resources to vms

--- domains.tf
+++ domains.tf
@@ -1,9 +1,20 @@
 module "ceph_domains" {
   source            = "github.com/evindunn/terraform-kvm-module"
   hostname_prefix   = "ceph"
+  cpu_count         = 2
+  ram_size          = 4096
   ssh_public_key    = file(local_file.ssh_key_public.filename)
   node_count        = 3
   network_id        = libvirt_network.bridge.id
+  mac_addresses     = [
+    "54:52:00:00:01:00",
+    "54:52:00:00:01:01",
+    "54:52:00:00:01:02",
+  ]
+  data_volumes      = {
+    count = 3
+    size  = 5369000000 # 5GiB
+  }
   ansible_playbook  = templatefile(
     "${path.module}/files/ansible-ceph-prepare.yml",
     {
@@ -14,25 +25,16 @@ module "ceph_domains" {
     {
       hostname      = "ceph0"
       path          = "/var/opt/id_ed25519"
-      owner         = "debian:debian"
-      permissions   = "0600"
       content       = tls_private_key.ceph_key_pair.private_key_pem
     }
   ]
-  mac_addresses     = [
-    "54:52:00:00:01:00",
-    "54:52:00:00:01:01",
-    "54:52:00:00:01:02",
-  ]
-  data_volumes      = {
-    count = 3
-    size  = 5369000000 # 5GiB
-  }
 }
 
 module "k8s_domains" {
   source            = "github.com/evindunn/terraform-kvm-module"
   hostname_prefix   = "k8s"
+  cpu_count         = 2
+  ram_size          = 4096
   ssh_public_key    = file(local_file.ssh_key_public.filename)
   node_count        = 3
   ansible_playbook  = file("${path.module}/files/ansible-k8s-prepare.yml")
