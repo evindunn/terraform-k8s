cf14d094cf5b71da654ce038277b44cef23cd3af
Evin Dunn <evindunn14@gmail.com>
2021-11-13 21:02:13 -0700

    Adds internal net search domain

--- auth.tf
+++ auth.tf
@@ -3,3 +3,17 @@ resource "tls_private_key" "ceph_key_pair" {
   ecdsa_curve = "P256"
 }
 
+# resource "null_resource" ceph_key_provisioner {
+#   connection {
+#     host  = "ceph0"
+#     type  = "ssh"
+#     user  = "debian"
+#     private_key = file(element(split(".", var.ssh_key_public), 0))
+#   }
+
+#   provisioner "file" {
+#     content     = tls_private_key.ceph_key_pair.private_key_pem
+#     destination = "/home/debian/.ssh/id_ed25519"
+#   }  
+# }
+
